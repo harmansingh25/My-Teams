6505722a37f5dd9fed23930932f62b9a53759fb9
Harman Singh <harman19042@iiitd.ac.in>
2021-07-02 03:25:20 +0530

    make it better

--- server.js
+++ server.js
@@ -32,4 +32,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(process.env.PORT || 3030, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
